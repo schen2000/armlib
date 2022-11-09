6fac66869aa69c06d0469cec7f05a401d0a16d69
Sherman Chen <schen@simviu.com>
2022-11-09 22:56:16 +0800

    [z1]: conversion

--- src/armZ1.cpp
+++ src/armZ1.cpp
@@ -3,7 +3,19 @@
 using namespace arm;
 using namespace unitree;
 
-
+namespace{
+   Posture conv(const Trans& T)
+   {
+      Posture p;
+      p.pitch = T.e.p;
+      p.roll = T.e.r;
+      p.yaw = T.e.y;
+      p.x = T.t.x();
+      p.y = T.t.y();
+      p.z = T.t.z();
+      return p;
+   }
+}
 
 
 //-----
@@ -12,8 +24,7 @@ bool ArmZ1::moveTo(const TipSt& t, int type)
     
     //posture[0] << 0,0,0,0.45,-0.2,0.2;
     auto& T = t.T;
-  //  Euler e(T.q);
-  //  MoveL(posture[0]);
+    uarm_.MoveL(PosturetoVec6(conv(T)));
 
     return true;
 }
